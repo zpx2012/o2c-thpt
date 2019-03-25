e956551bfbd98c41139c7e77dab090935cca2892
zpx2012 <zhupengxiong@yeah.net>
2019-03-25 00:00:09 -0700

    fix

--- rsync.sh
+++ rsync.sh
@@ -4,6 +4,6 @@ chmod 400 tmp_rsa
 outd=/data/tmp/st-data/$(hostname)
 ssh -oStrictHostKeyChecking=no -i tmp_rsa tmp@terran.cs.ucr.edu mkdir -p $outd
 while true; do
-    rsync -avzS --progress --remove-source-files rs/* tmp@terran.cs.ucr.edu:$outd/
+    rsync -avzS --progress --remove-source-files --rsh='ssh -oStrictHostKeyChecking=no -i tmp_rsa' rs/* tmp@terran.cs.ucr.edu:$outd/
     sleep 600
 done
