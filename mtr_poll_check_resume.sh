d5788b645b6530c6b256d3893712f819f0ae2f92
zpx2012 <zhupengxiong@yeah.net>
2019-03-27 17:49:24 -0700

    fix

--- mtr_poll_check_resume.sh
+++ mtr_poll_check_resume.sh
@@ -14,13 +14,13 @@ while true;do
                 let max=19
             fi
             echo $ip $hn $dp $sp $((max+1)) >> $fm
-            cat tmp >> ~/sanity_test/rs/mtr_data_$(hostname)_${hn}_${sp}_tcp_0.1_50_check_${start}.txt
+            cat tmp >> ~/o2c-thpt/rs/mtr_data_$(hostname)_${hn}_${sp}_tcp_0.1_50_check_${start}.txt
         done
         let i=0    
     fi
     cat $fm | while IFS=' ' read ip hn dp sp max; do
         echo $ip $hn $dp $sp
-        sudo $mtr -zwnr4T -m $max -P $dp -c 100 $ip 2>&1 | tee -a ~/sanity_test/rs/mtr_data_$(hostname)_${hn}_${sp}_tcp_1_100_${start}.txt
+        sudo $mtr -zwnr4T -m $max -P $dp -c 100 $ip 2>&1 | tee -a ~/o2c-thpt/rs/mtr_data_$(hostname)_${hn}_${sp}_tcp_1_100_${start}.txt
     done
     let i=i+1
 done
