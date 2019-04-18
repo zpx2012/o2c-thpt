f5a8c679a9c951671e363d9155db3ad56564d470
zpx2012 <zhupengxiong@yeah.net>
2019-04-18 01:19:21 -0700

    modified:   rsync.sh modified:   rsync_rm_pcap.sh

--- rsync.sh
+++ rsync.sh
@@ -4,6 +4,6 @@ chmod 400 tmp_rsa
 outd=/data/tmp/st-data/$(hostname)
 ssh -oStrictHostKeyChecking=no -i tmp_rsa tmp@terran.cs.ucr.edu mkdir -p $outd
 while true; do
-    rsync -avzS --progress --rsh='ssh -oStrictHostKeyChecking=no -i tmp_rsa' rs/* tmp@terran.cs.ucr.edu:$outd/
+    rsync -avzS --progress --rsh='ssh -oStrictHostKeyChecking=no -i tmp_rsa' ~/sanity_test/rs/* tmp@terran.cs.ucr.edu:$outd/
     sleep 3600
 done
--- rsync_rm_pcap.sh
+++ rsync_rm_pcap.sh
@@ -4,6 +4,6 @@ chmod 400 tmp_rsa
 outd=/data/tmp/st-data/$(hostname)
 ssh -oStrictHostKeyChecking=no -i tmp_rsa tmp@terran.cs.ucr.edu mkdir -p $outd
 while true; do
-    rsync -avzS --remove-source-files --progress --rsh='ssh -oStrictHostKeyChecking=no -i tmp_rsa' rs/tcpdump_* tmp@terran.cs.ucr.edu:$outd/
+    rsync -avzS --remove-source-files --progress --rsh='ssh -oStrictHostKeyChecking=no -i tmp_rsa' ~/sanity_test/rs/tcpdump_* tmp@terran.cs.ucr.edu:$outd/
     sleep 3600
 done
